dcadf9152b17a460026650c57d102b7f981f7cb4
root <root@jea-pc0x.localdomain>
2023-04-27 08:00:17 +0200

    weather adddress updated after moving to render

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = '';
     messageTwo.textContent = '';
 
-    fetch('http://localhost:3000/Weather?address=' + location).then((response) => {
+    fetch('https://nodejs-weather-website.onrender.com/Weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error;
